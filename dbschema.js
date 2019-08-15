58c272ae445a1b6e56573f567a93dd70d208eb82
shaunakturaga <shaunakturaga@gmail.com>
2019-08-15 02:48:14 -0400

    created routes, login, validation for api

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let schema = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream body',
+            createdAt: '2019-08-15T02:06:41.448Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
